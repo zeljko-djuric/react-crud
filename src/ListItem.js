c987e06687320df268e4425c0cc4ef5d4a8c34a1
Zeljko Djuric <zeljko94@gmail.com>
2018-10-12 13:05:16 +0200

    Changed button position and text

--- src/ListItem.js
+++ src/ListItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ListItem = (props) =>{
     return <li className="list-group-item">
-    <button className="btn-sm mr-4 btn btn-info" onClick={props.editTodo}>U</button>
     {props.item.name}
-    <button className="btn-sm ml-4 btn btn-danger" onClick={props.deleteTodo}>X</button>
+    <button className="btn-sm ml-4 btn btn-info" onClick={props.editTodo}>Update</button>
+    <button className="btn-sm ml-1 btn btn-danger" onClick={props.deleteTodo}>Delete</button>
     </li>
 
 };
